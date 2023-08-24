836d909f061e302448cfcf59dd7a1b96ee5b63e9
MeiVinEight <3390038158@qq.com>
2023-08-24 13:05:22 +0800

    [CMD] Global variables

---
+++ src/Transportation.h
@@ -0,0 +1,11 @@
+#ifndef TRANSPORTATION_TRANSPORTATION_H
+#define TRANSPORTATION_TRANSPORTATION_H
+
+#include "CommandManager.h"
+
+namespace Transportation
+{
+	Transportation::CommandManager command;
+}
+
+#endif //TRANSPORTATION_TRANSPORTATION_H
