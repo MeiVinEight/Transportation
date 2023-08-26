9fab6ec38946f4e5ac3acff2af25ed924e5d7cad
夏雨 <3390038158@qq.com>
2023-08-26 19:32:26 +0800

    [CONSOLE] Runtime constant of streaming

--- src/Transportation.h
+++ src/Transportation.h
@@ -1,11 +1,15 @@
 #ifndef TRANSPORTATION_TRANSPORTATION_H
 #define TRANSPORTATION_TRANSPORTATION_H
 
+#include <standardstream.h>
+
 #include "CommandManager.h"
+#include "Tstreaming.h"
 
 namespace Transportation
 {
 	Transportation::CommandManager command;
+	Transportation::streaming cout(&Streaming::cout);
 }
 
 #endif //TRANSPORTATION_TRANSPORTATION_H
