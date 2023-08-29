d48a14c0a1eba1031d12f36b4f64216140546d30
夏雨 <3390038158@qq.com>
2023-08-29 09:52:52 +0800

    [NETWORK] Use stream reference

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -1,11 +1,13 @@
+#include <stream.h>
+
 #include "protocol.h"
 #include "datapack.h"
 
 template<typename T>
-Transportation::packet::Datapack *ctor(BYTE *data)
+Transportation::packet::Datapack *ctor(Streaming::stream &stream)
 {
-	return (Transportation::packet::Datapack *) (new T(data));
+	return (Transportation::packet::Datapack *) (new T(stream));
 }
 
-Transportation::packet::Datapack *(*(Transportation::protocol::datapack[]))(BYTE *) = {
+Transportation::packet::Datapack *(*(Transportation::protocol::datapack[]))(Streaming::stream &) = {
 };
--- src/protocol.h
+++ src/protocol.h
@@ -9,7 +9,7 @@ namespace Transportation
 {
 	namespace protocol
 	{
-		extern Transportation::packet::Datapack *(*(datapack[]))(BYTE *);
+		extern Transportation::packet::Datapack *(*(datapack[]))(Streaming::stream &);
 	}
 }
 #endif //TRANSPORTATION_PROTOCOL_H
