693e84aa77237eb93f778c7244d1abf5cdf606f7
夏雨 <3390038158@qq.com>
2023-08-30 13:17:40 +0800

    [PROTOCOL] Handshaking

---
+++ src/Handshaking.h
@@ -0,0 +1,25 @@
+#ifndef TRANSPORTATION_HANDSHAKING_H
+#define TRANSPORTATION_HANDSHAKING_H
+
+#include <sstring.h>
+
+#include "datapack.h"
+
+namespace Transportation
+{
+	namespace packet
+	{
+		class Handshaking: public Transportation::packet::Datapack
+		{
+			public:
+			WORD version;
+			String::string name;
+			Handshaking();
+			void operator()(Transportation::ConnectionManager &) const override;
+			Transportation::packet::Handshaking &operator<<(Streaming::stream &) override;
+			Transportation::packet::Handshaking &operator>>(Streaming::stream &) override;
+		};
+	} // Transportation
+} // packet
+
+#endif //TRANSPORTATION_HANDSHAKING_H
