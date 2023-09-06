6a05ac6e4fd6eec3eb0be937daff8aa4fd3caebe
夏雨 <3390038158@qq.com>
2023-09-06 22:16:23 +0800

    [PROTOCOL] Call destructor when stop

--- src/CommandStop.cpp
+++ src/CommandStop.cpp
@@ -6,7 +6,6 @@ Transportation::CommandStop::CommandStop(): Transportation::CMD("stop")
 }
 bool Transportation::CommandStop::operator()(const String::string &) const
 {
-	Transportation::network.opening = false;
-	Transportation::network.server.close();
+	Transportation::network.~NetworkManager();
 	return false;
 }
