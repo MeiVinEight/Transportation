0875d330dba58d5844840568108e642c606ca0f7
夏雨 <3390038158@qq.com>
2023-09-22 09:10:51 +0800

    [PROTOCOL] Add packet(s) into protocol

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -2,6 +2,8 @@
 #include "datapack.h"
 #include "Handshaking.h"
 #include "Disconnect.h"
+#include "FileSearch.h"
+#include "FileReply.h"
 
 template<typename T>
 Transportation::packet::Datapack *ctor()
@@ -11,6 +13,8 @@ Transportation::packet::Datapack *ctor()
 
 Transportation::packet::Datapack *(*(Transportation::protocol::datapack[]))() = {
 	ctor<Transportation::packet::Handshaking>,
-	ctor<Transportation::packet::Disconnect>
+	ctor<Transportation::packet::Disconnect>,
+	ctor<Transportation::packet::FileSearch>,
+	ctor<Transportation::packet::FileReply>,
 };
 WORD Transportation::protocol::version = 0;
